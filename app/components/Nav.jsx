7a604af4c3c106271ab503b418d5161a9c35691d
Kaleb Cooper <kalebcooper@gmail.com>
2017-05-21 22:39:03 +1200

    Add ErrorModal

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -11,7 +11,7 @@ var Nav = React.createClass({
             <div className="top-bar">
                 <div className="top-bar-left">
                     <ul className="menu">
-                        <li className="menu-text">Kal Weather</li>
+                        <li className="menu-text">Weather by Kal</li>
                         <li>
                             <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
                         </li>
