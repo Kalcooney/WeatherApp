ba8b590e0758c54e349ccd92d6c0e8564f941260
Kaleb Cooper <kalebcooper@gmail.com>
2017-05-21 23:01:50 +1200

    Add custom styling

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h1 className="text-center">About</h1>
+            <h1 className="text-center page-title">About</h1>
             <h4 className="text-center">What is Kal Weather?</h4>
             <p>Kal Weather is a web application that allows you to search for a city and get back the current temperature
                 for that city. <br /><br />
--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -13,7 +13,7 @@ var WeatherForm = React.createClass({
         return(
             <div>
                 <form onSubmit={this.onFormSubmit}>
-                    <input type="text" ref="location" placeholder="Search Weather"/>
+                    <input type="search" ref="location" placeholder="Search Weather"/>
                     <button className="hollow button expanded">Get Weather</button>
                 </form>
             </div>
